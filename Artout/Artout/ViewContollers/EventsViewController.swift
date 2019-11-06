c759602e6911dcf9c39f7c1625bf81b06ed3d3f4
Pooya Kabiri <pooya_kabiri@yahoo.com>
2019-11-06 21:22:17 +0330

    Add EventsViewController

---
+++ Artout/Artout/ViewContollers/EventsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EventsViewController.swift
+//  Artout
+//
+//  Created by Pooya Kabiri on 11/6/19.
+//  Copyright © 2019 Pooya Kabiri. All rights reserved.
+//
+
+import UIKit
+
+class EventsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
