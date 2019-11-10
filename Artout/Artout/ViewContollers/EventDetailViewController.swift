246ff39dd4ad0ac87a0fa4dd9db616c6ef801b5f
Alireza <nili_505077@yahoo.comgit config --global user.email nili_505077@yahoo.com>
2019-11-11 03:02:44 +0330

    Implement EventDetailViewController

--- Artout/Artout/ViewContollers/EventDetailViewController.swift
+++ Artout/Artout/ViewContollers/EventDetailViewController.swift
@@ -6,4 +6,34 @@
 //  Copyright © 2019 Pooya Kabiri. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+class EventDetailViewController: UIViewController{
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    var eventDetailViewModel = EventDetailViewModel()
+    var disposeBag = DisposeBag()
+    var eventId = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        eventDetailViewModel.RequestEventDetail(id: eventId)
+        
+        self.eventDetailViewModel.event.subscribe { event in
+            DispatchQueue.main.async {
+                self.titleLabel.text = event.element!.title
+                self.categoryLabel.text = event.element!.category
+                self.dateLabel.text = "From: \(event.element!.start_date) To: \(event.element!.end_date)"
+                self.descriptionTextView.text = event.element!.description
+            }
+        }.disposed(by: disposeBag)
+    }
+}
