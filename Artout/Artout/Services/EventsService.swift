40e40e4724e3f9e56af20d7b7640f3dfc70e3212
Alireza <nili_505077@yahoo.comgit config --global user.email nili_505077@yahoo.com>
2019-11-11 02:48:23 +0330

    Implement event detail network request

--- Artout/Artout/Services/EventsService.swift
+++ Artout/Artout/Services/EventsService.swift
@@ -100,6 +100,42 @@ class EventsService {
         })
     }
     
+    func RequestEventDetail(id: Int) -> Single<EventResponse>{
+        
+        return Single<EventResponse>.create(subscribe: { single in
+            Observable.from(optional: [String].self)
+                .map {_ in
+                    let url = URL(string: Endpoint.GCPServer.rawValue + APIPaths.EventDetail.rawValue + "\(id)/")!
+                    var request = URLRequest(url: url)
+                    request.httpMethod = HTTPMethod.GET.rawValue
+                    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+                    return request
+            }
+            .flatMap { request in
+                URLSession.shared.rx.response(request: request)
+            }
+            .subscribe(onNext: { [weak self] response, data in
+                guard response.statusCode == 200 else{
+                    DispatchQueue.main.async {
+                        single(.error(NetworkingError.InternalServerError))
+                    }
+                    return
+                }
+                guard let responseDTO = self!.Formatter.Decode(data: data) else {
+                    DispatchQueue.main.async {
+                        single(.error(NetworkingError.InternalServerError))
+                    }
+                    return
+                }
+                single(.success(responseDTO))
+                return
+                }, onError: { error in
+                    print(error.localizedDescription)
+            }).disposed(by: self.disposeBag)
+            return Disposables.create()
+        })
+    }
+    
     func convertDate(date: String) -> String{
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "EEEE, MMM d, yyyy"
