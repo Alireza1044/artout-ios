b4f0fcb0996b6adf190237cb5d6af39ec1a4a54e
Pooya Kabiri <pooya_kabiri@yahoo.com>
2019-11-10 16:16:37 +0330

    Implement Request Event Service

--- Artout/Artout/Services/EventsService.swift
+++ Artout/Artout/Services/EventsService.swift
@@ -15,8 +15,43 @@ class EventsService {
     let isLoading = PublishSubject<Bool>()
     let disposeBag = DisposeBag()
     
-    func RequestEventsIDs () {
-        //        let rawData
+    func RequestEvents () -> Single<[EventResponse]> {
+        return Single<[EventResponse]>.create(subscribe: { single in
+            Observable.from(optional: [String].self)
+                .map {_ in
+                    let url = URL(string: Endpoint.GCPServer.rawValue + APIPaths.FetchEvents.rawValue)!
+                    var request = URLRequest(url: url)
+                    request.httpMethod = HTTPMethod.GET.rawValue
+                    //                    request.httpBody = self.Formatter.Encode(objDTO: rawData)
+                    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+                    return request
+            }
+            .flatMap { request in
+                URLSession.shared.rx.response(request: request)
+            }
+            .subscribe(onNext: { [weak self] response, data in
+                guard response.statusCode == 200 else{
+                    DispatchQueue.main.async {
+                        //                        self!.isLoading.onNext(false)
+                        single(.error(NetworkingError.CredenttialsNotValid))
+                    }
+                    return
+                }
+                let decoder = JSONDecoder()
+                guard let responseDTO = try? decoder.decode([EventResponse].self, from: data) else {
+                    DispatchQueue.main.async {
+                        //                        self!.isLoading.onNext(false)
+                        single(.error(NetworkingError.InternalServerError))
+                    }
+                    return
+                }
+                single(.success(responseDTO))
+                return
+                }, onError: { error in
+                    print(error.localizedDescription)
+            }).disposed(by: self.disposeBag)
+            return Disposables.create()
+        })
     }
     
     func AddEvents (title:String, category: String,description:String,start_date:String,end_date:String,picture_url:String,event_owner:Int,location:[String:Float] = ["longitude":0.0,"latitude":0.0]) -> Single<String> {
