11a3e9848ec72f115e57e9e048fd8b8b116bba47
Alireza <nili_505077@yahoo.comgit config --global user.email nili_505077@yahoo.com>
2019-11-11 02:47:17 +0330

    Design event description UI

---
+++ Artout/Artout/ViewContollers/EventDetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  EventDetailViewController.swift
+//  Artout
+//
+//  Created by Alireza Moradi on 11/10/19.
+//  Copyright © 2019 Pooya Kabiri. All rights reserved.
+//
+
+import Foundation
---
+++ Artout/Artout/ViewModels/EventDetailViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  EventDetailViewModel.swift
+//  Artout
+//
+//  Created by Alireza Moradi on 11/11/19.
+//  Copyright © 2019 Pooya Kabiri. All rights reserved.
+//
+
+import Foundation
