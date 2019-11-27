ce2be82b0a85a7433696c2df60437fb6dfc3bfff
Alireza <nili_505077@yahoo.comgit config --global user.email nili_505077@yahoo.com>
2019-11-28 00:47:00 +0330

    Implement edit event network request

--- Artout/Artout/Services/EventsService.swift
+++ Artout/Artout/Services/EventsService.swift
@@ -102,6 +102,8 @@ class EventsService {
     
     func RequestEventDetail(id: Int) -> Single<EventResponse>{
         
+        self.isLoading.onNext(true)
+        
         return Single<EventResponse>.create(subscribe: { single in
             Observable.from(optional: [String].self)
                 .map {_ in
@@ -118,16 +120,67 @@ class EventsService {
                 guard response.statusCode == 200 else{
                     DispatchQueue.main.async {
                         single(.error(NetworkingError.InternalServerError))
+                        self?.isLoading.onNext(false)
                     }
                     return
                 }
                 guard let responseDTO = self!.Formatter.Decode(data: data) else {
                     DispatchQueue.main.async {
                         single(.error(NetworkingError.InternalServerError))
+                        self?.isLoading.onNext(false)
                     }
                     return
                 }
+                DispatchQueue.main.async {
+                    self?.isLoading.onNext(false)
+                }
                 single(.success(responseDTO))
+                return
+                }, onError: { error in
+                    print(error.localizedDescription)
+            }).disposed(by: self.disposeBag)
+            return Disposables.create()
+        })
+    }
+    
+    func EditEvent (id: Int, title:String, category: String,description:String,start_date:String,end_date:String,picture_url:String,event_owner:Int,location:LocationEntity = LocationEntity(latitude: 0.0, longitude: 0.0)) -> Single<String> {
+        
+        isLoading.onNext(true)
+        
+        let rawData = EventModel(title: title, category: category, description: description, start_date: convertDate(date: start_date), end_date: convertDate(date: end_date), picture_url: picture_url, event_owner: event_owner, location: location)
+        
+        return Single<String>.create(subscribe: { single in
+            Observable.from(optional: [String].self)
+                .map {_ in
+                    let url = URL(string: Endpoint.GCPServer.rawValue + APIPaths.EventDetail.rawValue + "\(id)/")!
+                    var request = URLRequest(url: url)
+                    request.httpMethod = HTTPMethod.PUT.rawValue
+                    request.httpBody = self.Formatter.Encode(objDTO: rawData)
+                    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+                    return request
+            }
+            .flatMap { request in
+                URLSession.shared.rx.response(request: request)
+            }
+            .subscribe(onNext: { [weak self] response, data in
+                guard response.statusCode == 200 else{
+                    DispatchQueue.main.async {
+                        self!.isLoading.onNext(false)
+                        single(.error(NetworkingError.CredenttialsNotValid))
+                    }
+                    return
+                }
+                guard let responseDTO = self!.Formatter.Decode(data: data) else {
+                    DispatchQueue.main.async {
+                        self!.isLoading.onNext(false)
+                        single(.error(NetworkingError.InternalServerError))
+                    }
+                    return
+                }
+                single(.success(responseDTO.title))
+                DispatchQueue.main.async {
+                    self!.isLoading.onNext(false)
+                }
                 return
                 }, onError: { error in
                     print(error.localizedDescription)
