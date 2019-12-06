66cb24415401bb651d08f41f2cb8bfde7322be6a
Pooya Kabiri <pooya_kabiri@yahoo.com>
2019-12-06 19:47:31 +0330

    Add TableViewControllers for Follower and Following Lists

---
+++ Artout/Artout/ViewContollers/MyFollowersTableViewController.swift
@@ -0,0 +1,92 @@
+//
+//  MyFollowersTableViewController.swift
+//  Artout
+//
+//  Created by Pooya Kabiri on 12/6/19.
+//  Copyright © 2019 Pooya Kabiri. All rights reserved.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+
+class MyFollowersTableViewController: UITableViewController {
+
+    var viewModel = MyFollowersViewModel()
+    var disposeBag = DisposeBag()
+    var refreshControlInstance = UIRefreshControl()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.Followers.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FollowersCell", for: indexPath) as! FollowersAndFollowingTableViewCell
+        if viewModel.Followers.count > 0 {
+            let item = viewModel.Followers[indexPath.row]
+            ConfigureCell(for: cell, with: item)
+        }
+        return cell
+    }
+
+    func ConfigureCell(for cell: FollowersAndFollowingTableViewCell, with item: UserEntity) {
+        cell.FFFullName?.text = item.FullName
+    }
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
