2c68879eff77c3418c6e2bf971209a0d48710c40
Alireza <nili_505077@yahoo.comgit config --global user.email nili_505077@yahoo.com>
2019-12-08 00:59:19 +0330

    Implement profile view animation logic

--- Artout/Artout/ViewContollers/ProfileViewController.swift
+++ Artout/Artout/ViewContollers/ProfileViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var headerViewHeightConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var saulehImage: UIImageView!
+    
+    let headerViewMaxHeight: CGFloat = 320
+    var headerViewMinHeight: CGFloat = 97 + (UIApplication.shared.windows.filter {$0.isKeyWindow}.first?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0)
+    
+    var alpha: CGFloat = 0.0
+    var height: CGFloat = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        alpha = saulehImage.alpha
+        height = saulehImage.frame.size.height / 2
+        self.saulehImage.layer.cornerRadius = height;
+        self.saulehImage.clipsToBounds = true;
+        self.saulehImage.layer.borderWidth = 3.0;
+        self.saulehImage.layer.borderColor = UIColor .white.cgColor;
     }
-    
-
 }
+
+extension ProfileViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let y: CGFloat = scrollView.contentOffset.y
+        let newHeaderViewHeight: CGFloat = headerViewHeightConstraint.constant - y
+        
+        if newHeaderViewHeight > headerViewMaxHeight {
+            headerViewHeightConstraint.constant = headerViewMaxHeight
+            saulehImage.alpha = (alpha * (headerViewHeightConstraint.constant) / headerViewMaxHeight)
+        } else if newHeaderViewHeight < headerViewMinHeight {
+            headerViewHeightConstraint.constant = headerViewMinHeight
+            saulehImage.alpha = (alpha * (headerViewHeightConstraint.constant - headerViewMinHeight) / headerViewMaxHeight)
+            
+        } else {
+            headerViewHeightConstraint.constant = newHeaderViewHeight
+            scrollView.contentOffset.y = 0 // block scroll view
+            saulehImage.alpha = (alpha * (headerViewHeightConstraint.constant) / headerViewMaxHeight)
+        }
+    }
+}
+
