02b4c44a67669347c91348d9ccb9629e84f3056d
Alireza <nili_505077@yahoo.comgit config --global user.email nili_505077@yahoo.com>
2019-12-15 15:14:31 +0330

    Change event detail labels

--- Artout/Artout/ViewContollers/EventDetailViewController.swift
+++ Artout/Artout/ViewContollers/EventDetailViewController.swift
@@ -13,15 +13,16 @@ import RxCocoa
 class EventDetailViewController: UIViewController{
     
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var titleLabel: CustomLabel!
-    @IBOutlet weak var categoryLabel: CustomLabel!
-    @IBOutlet weak var dateLabel: CustomLabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var startDate: CustomLabel!
+    @IBOutlet weak var endDate: CustomLabel!
     @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var editButton: UIBarButtonItem!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
-    var startDate = ""
-    var endDate = ""
+    var startDateStr = ""
+    var endDateStr = ""
     var eventDetailViewModel = EventDetailViewModel()
     var disposeBag = DisposeBag()
     var eventId = 0
@@ -29,20 +30,20 @@ class EventDetailViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleLabel.layer.borderColor = UIColor.darkGray.cgColor
-        titleLabel.layer.borderWidth = 1
-        titleLabel.layer.cornerRadius = 4
+        
         //        titleLabel.layer.masksToBounds = true
         
         
-        dateLabel.layer.borderColor = UIColor.darkGray.cgColor
-        dateLabel.layer.borderWidth = 1
-        dateLabel.layer.cornerRadius = 4
+        startDate.layer.borderColor = UIColor.darkGray.cgColor
+        startDate.layer.borderWidth = 1
+        startDate.layer.cornerRadius = 4
         //        dateLabel.layer.masksToBounds = true
         
-        categoryLabel.layer.borderColor = UIColor.darkGray.cgColor
-        categoryLabel.layer.borderWidth = 1
-        categoryLabel.layer.cornerRadius = 4
+        endDate.layer.borderColor = UIColor.darkGray.cgColor
+        endDate.layer.borderWidth = 1
+        endDate.layer.cornerRadius = 4
+        
+        
         //        categoryLabel.layer.masksToBounds = true
         
         self.descriptionTextView.textContainerInset = .init(top: 2, left: 4, bottom: 0, right: 4)
@@ -71,9 +72,8 @@ class EventDetailViewController: UIViewController{
             DispatchQueue.main.async {
                 self.titleLabel.text = event.element!.Title
                 self.categoryLabel.text = event.element!.Category
-                self.startDate = event.element!.StartDate
-                self.endDate = event.element!.EndDate
-                self.dateLabel.text = "From: \(self.startDate) To: \(self.endDate)"
+                self.startDate.text = event.element!.StartDate
+                self.endDate.text = event.element!.EndDate
                 self.descriptionTextView.text = event.element!.Description
             }
         }.disposed(by: disposeBag)
@@ -85,8 +85,8 @@ class EventDetailViewController: UIViewController{
             editEventVC.eventId = self.eventId
             editEventVC.titleText = self.titleLabel.text!
             editEventVC.descriptionText = self.descriptionTextView.text
-            editEventVC.startDateText = editEventVC.convertDate(date: self.startDate)
-            editEventVC.endDateText = editEventVC.convertDate(date: self.endDate)
+            editEventVC.startDateText = editEventVC.convertDate(date: self.startDate.text!)
+            editEventVC.endDateText = editEventVC.convertDate(date: self.endDate.text!)
             editEventVC.categoryText = self.categoryLabel.text!
         }
     }
