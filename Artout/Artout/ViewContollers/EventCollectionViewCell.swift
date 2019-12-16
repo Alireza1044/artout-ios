0bd8d333c067979e300ba79a785f81a4a62955cb
Pooya Kabiri <pooya_kabiri@yahoo.com>
2019-12-16 17:46:52 +0330

    Create My event Collection View

---
+++ Artout/Artout/ViewContollers/EventCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  EventCollectionViewCell.swift
+//  Artout
+//
+//  Created by Pooya Kabiri on 12/16/19.
+//  Copyright © 2019 Pooya Kabiri. All rights reserved.
+//
+
+import UIKit
+
+class EventCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+    
+}
