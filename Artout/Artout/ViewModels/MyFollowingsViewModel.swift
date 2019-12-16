5dba3a37acc3b37f91b206b47f079cf25e8f7fe7
Pooya Kabiri <pooya_kabiri@yahoo.com>
2019-12-16 23:24:08 +0330

    Add alert for showing adding friend status

--- Artout/Artout/ViewModels/MyFollowingsViewModel.swift
+++ Artout/Artout/ViewModels/MyFollowingsViewModel.swift
@@ -10,18 +10,31 @@ import Foundation
 import RxSwift
 
 class MyFollowingsViewModel {
-    
+    let refresh: PublishSubject<Bool>
     var disposeBag = DisposeBag()
     var Followings: [UserEntity]
     let service = FriendService()
     
     init() {
         self.Followings = []
+        self.refresh = PublishSubject<Bool>()
+
     }
-    
+    func ChangeState(state: Bool, id: String) {
+        if state {
+            _ = service.Unfollow(with: id).subscribe(onSuccess: { (bool) in
+                self.refresh.onNext(true)
+            })
+        } else {
+            _ = service.AddFriend(id: id).subscribe(onSuccess: { (bool) in
+                self.refresh.onNext(true)
+            })
+        }
+    }
     func FetchFollowings() {
         _ = service.fetchFollowingList().subscribe(onSuccess: { data in
             self.Followings = data
+            self.refresh.onNext(true)
         })
     }
 }
