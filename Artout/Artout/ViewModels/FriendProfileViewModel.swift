88bcc874fb1e814b3cb4a5120c53eafa7b49edaa
Alireza <nili_505077@yahoo.comgit config --global user.email nili_505077@yahoo.com>
2019-12-25 15:59:59 +0330

    Implement friend profile view model

--- Artout/Artout/ViewModels/FriendProfileViewModel.swift
+++ Artout/Artout/ViewModels/FriendProfileViewModel.swift
@@ -7,3 +7,53 @@
 //
 
 import Foundation
+import RxSwift
+
+class FriendProfileViewModel{
+    
+    var followerCount: BehaviorSubject<String>
+    var followingCount: BehaviorSubject<String>
+    var name: BehaviorSubject<String>
+    var username: BehaviorSubject<String>
+    var state: BehaviorSubject<String>
+    
+    var isLoading: PublishSubject<Bool>
+    var profile: BehaviorSubject<FriendProfileEntity>
+    
+    var service = ProfileService()
+    var disposeBag = DisposeBag()
+    
+    init() {
+        followerCount = BehaviorSubject<String>(value: "")
+        followingCount = BehaviorSubject<String>(value: "")
+        name = BehaviorSubject<String>(value: "")
+        username = BehaviorSubject<String>(value: "")
+        state = BehaviorSubject<String>(value: "")
+        isLoading = PublishSubject<Bool>()
+        profile = BehaviorSubject<FriendProfileEntity>(value: FriendProfileEntity(FirstName: "",
+                                                                                  LastName: "",
+                                                                                  UserName: "",
+                                                                                  Avatar: "",
+                                                                                  FollowerCount: "",
+                                                                                  FollowingCount: "",
+                                                                                  Id: -1))
+    }
+    
+    func requestProfileDetail(id: Int){
+        
+        service.isLoading.subscribe({
+            switch $0{
+            case .next(true):
+                self.isLoading.on(.next(true))
+            case .next(false):
+                self.isLoading.on(.next(false))
+            default:
+                self.isLoading.on(.next(false))
+            }
+        }).disposed(by: disposeBag)
+        
+        service.RequestEventDetail(id: id).subscribe(onSuccess: { (friendProfile) in
+            self.profile.onNext(friendProfile)
+        }, onError: self.profile.onError(_:)).disposed(by: disposeBag)
+    }
+}
