be686ddfbbeb9bf1eb02bd88c423f0253ded6538
Alireza <nili_505077@yahoo.comgit config --global user.email nili_505077@yahoo.com>
2019-12-25 16:00:41 +0330

    Implement friend profile service

--- Artout/Artout/Services/ProfileService.swift
+++ Artout/Artout/Services/ProfileService.swift
@@ -7,3 +7,54 @@
 //
 
 import Foundation
+import RxSwift
+
+class ProfileService{
+    
+    let isLoading = PublishSubject<Bool>()
+    let disposeBag = DisposeBag()
+    
+    func RequestEventDetail(id: Int) -> Single<FriendProfileEntity> {
+        
+        self.isLoading.onNext(true)
+        
+        return Single<FriendProfileEntity>.create(subscribe: { single in
+            Observable.from(optional: [String].self)
+                .map {_ in
+                    let url = URL(string: Endpoint.GCPServer.rawValue + APIPaths.AddEvent.rawValue + "\(id)/")! // Edit endpoints
+                    var request = URLRequest(url: url)
+                    request.httpMethod = HTTPMethod.GET.rawValue // edit http method
+                    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+                    request.addValue("Bearer " + UserDefaults.standard.string(forKey: "AccessToken")!, forHTTPHeaderField: "Authorization")
+                    return request
+            }
+            .flatMap { request in
+                URLSession.shared.rx.response(request: request)
+            }
+            .subscribe(onNext: { [weak self] response, data in
+                guard response.statusCode == 200 else{
+                    DispatchQueue.main.async {
+                        single(.error(HTTPStatusCodes.InternalServerError))
+                        self?.isLoading.onNext(false)
+                    }
+                    return
+                }
+                guard let profile = try? JSONDecoder().decode(FriendProfileDTO.self, from: data) else {
+                    DispatchQueue.main.async {
+                        single(.error(HTTPStatusCodes.InternalServerError))
+                        self?.isLoading.onNext(false)
+                    }
+                    return
+                }
+                DispatchQueue.main.async {
+                    self?.isLoading.onNext(false)
+                }
+                single(.success(profile.ToEntity()))
+                return
+                }, onError: { error in
+                    print(error.localizedDescription)
+            }).disposed(by: self.disposeBag)
+            return Disposables.create()
+        })
+    }
+}
