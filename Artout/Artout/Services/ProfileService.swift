fac1a5817ed66012620e164a4c8b773540a5fc50
Pooya Kabiri <pooya_kabiri@yahoo.com>
2019-12-29 14:48:08 +0330

    Implement Request My Profile Method

--- Artout/Artout/Services/ProfileService.swift
+++ Artout/Artout/Services/ProfileService.swift
@@ -14,14 +14,58 @@ class ProfileService{
     let isLoading = PublishSubject<Bool>()
     let disposeBag = DisposeBag()
     
-    func RequestEventDetail(id: Int) -> Single<FriendProfileEntity> {
+    func RequestProfile(id: Int) -> Single<FriendProfileEntity> {
         
         self.isLoading.onNext(true)
         
         return Single<FriendProfileEntity>.create(subscribe: { single in
             Observable.from(optional: [String].self)
                 .map {_ in
-                    let url = URL(string: Endpoint.GCPServer.rawValue + APIPaths.AddEvent.rawValue + "\(id)/")! // Edit endpoints
+                    let url = URL(string: Endpoint.GCPServer.rawValue + "users/profile/?user=" + "\(id)/")!
+                    var request = URLRequest(url: url)
+                    request.httpMethod = HTTPMethod.GET.rawValue // edit http method
+                    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+                    request.addValue("Bearer " + UserDefaults.standard.string(forKey: "AccessToken")!, forHTTPHeaderField: "Authorization")
+                    return request
+            }
+            .flatMap { request in
+                URLSession.shared.rx.response(request: request)
+            }
+            .subscribe(onNext: { [weak self] response, data in
+                guard response.statusCode == 200 else{
+                    DispatchQueue.main.async {
+                        single(.error(HTTPStatusCodes.InternalServerError))
+                        self?.isLoading.onNext(false)
+                    }
+                    return
+                }
+                guard let profile = try? JSONDecoder().decode(FriendProfileDTO.self, from: data) else {
+                    DispatchQueue.main.async {
+                        single(.error(HTTPStatusCodes.InternalServerError))
+                        self?.isLoading.onNext(false)
+                    }
+                    return
+                }
+                DispatchQueue.main.async {
+                    self?.isLoading.onNext(false)
+                }
+                single(.success(profile.ToEntity()))
+                return
+                }, onError: { error in
+                    print(error.localizedDescription)
+            }).disposed(by: self.disposeBag)
+            return Disposables.create()
+        })
+    }
+    
+    func RequestMyProfile(id: Int) -> Single<FriendProfileEntity> {
+        
+        self.isLoading.onNext(true)
+        
+        return Single<FriendProfileEntity>.create(subscribe: { single in
+            Observable.from(optional: [String].self)
+                .map {_ in
+                    let url = URL(string: Endpoint.GCPServer.rawValue + "users/profile/")!
                     var request = URLRequest(url: url)
                     request.httpMethod = HTTPMethod.GET.rawValue // edit http method
                     request.addValue("application/json", forHTTPHeaderField: "Content-Type")
