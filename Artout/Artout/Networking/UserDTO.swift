a68e85c0deeb690a138247d1cf7de273bc3a0d76
Alireza <nili_505077@yahoo.comgit config --global user.email nili_505077@yahoo.com>
2019-12-31 00:50:44 +0330

    Refactor user and friend DTOs

--- Artout/Artout/Networking/UserDTO.swift
+++ Artout/Artout/Networking/UserDTO.swift
@@ -14,6 +14,7 @@ class UserDTO: Codable {
     var email: String?
     var date_joined: String?
     var is_private: Bool?
+    var state:  Int?
     var username: String
     var avatar: String
     var id: Int
@@ -26,7 +27,7 @@ class UserDTO: Codable {
         self.last_name = LastName
     }
     
-    init(FirstName: String, LastName: String, UserName: String, Avatar: String, Id: Int, Email: String?, DateJoined: String?, IsPrivate: Bool?) {
+    init(FirstName: String, LastName: String, UserName: String, Avatar: String, Id: Int, Email: String?, DateJoined: String?, IsPrivate: Bool?, State: Int?) {
         self.username = UserName
         self.id = Id
         self.avatar = Avatar
@@ -35,5 +36,6 @@ class UserDTO: Codable {
         self.email = Email
         self.date_joined = DateJoined
         self.is_private = IsPrivate
+        
     }
 }
