9813e5984eb0478fc5312f98aa552e91c1e84cbd
Pooya Kabiri <pooya_kabiri@yahoo.com>
2020-01-27 13:22:48 +0330

    Modify Login Service for App Starting

--- Artout/Artout/Services/LoginService.swift
+++ Artout/Artout/Services/LoginService.swift
@@ -50,4 +50,41 @@ class LoginService {
             return Disposables.create()
         })
     }
+    func RefreshToken () -> Single<String> {
+        let entity = RefreshDTO(Refresh: TokenHandler.GetRefreshToken())
+        return Single<String>.create(subscribe: { single in
+            Observable.from(optional: [String].self)
+                .map {_ in
+                    let url = URL(string: Endpoint.GCPServer.rawValue + APIPaths.Refresh.rawValue)!
+                    var request = URLRequest(url: url)
+                    request.httpMethod = HTTPMethod.POST.rawValue
+                    request.httpBody = try? JSONEncoder().encode(entity)
+                    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+                    return request
+            }
+            .flatMap { request in
+                URLSession.shared.rx.response(request: request)
+            }
+            .subscribe(onNext: { [weak self] response, data in
+                guard response.statusCode == 200 else{
+                    DispatchQueue.main.async {
+                        single(.error(HTTPStatusCodes.BadRequest))
+                    }
+                    return
+                }
+                guard let response = try? JSONDecoder().decode(RefreshResponseDTO.self, from: data) else {
+                    DispatchQueue.main.async {
+                        single(.error(HTTPStatusCodes.InternalServerError))
+                    }
+                    return
+                }
+                self?.TokenHandler.RenewAccessToken(access: response.access)
+                single(.success(response.access))
+                return
+                }, onError: { error in
+                    print(error.localizedDescription)
+            }).disposed(by: self.disposeBag)
+            return Disposables.create()
+        })
+    }
 }
